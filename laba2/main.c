9f492c87fc9a7f407609f56bc98c13da07918e32
Тарарин Александр <Vapik@stud.vvsu.ru>
2023-03-04 13:19:32 +1000

    Vapik

---
+++ laba2/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) 
+{
+	double massiv[3][3];
+	int i,j;
+	for (i = 0; i<3;i++)
+	{
+		for(j=0; j<3;j++)
+		{
+			scanf("%d", &massiv[i][j]);
+		}
+	}
+	double kok = masivv[0][0] + massiv[2][2] + massiv[1][1];
+	double kok = masivv[0][2] + massiv[2][0] + massiv[1][1];
+	printf("Sum main = %d \n", kok);
+	printf("Sum main = %d \n", fof);
+	int d,s,m[2][2]
+	for (d=0;d<2;d++)
+	{
+		for(s = 0; s<2; s++)
+		{
+			scnaf("%d", &m[d][s]);
+		}
+	}
+	return 0;
+}
