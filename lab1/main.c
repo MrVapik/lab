3c7ed996910656a03ff550c1953188f94ee910eb
Тарарин Александр <Vapik@stud.vvsu.ru>
2023-03-04 12:36:24 +1000

    123

--- lab1/main.c
+++ lab1/main.c
@@ -4,14 +4,29 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 	int a,b,c,d;
 	double x1, x2;
-	a=3; b=4; c=1;
+	scanf("%d", &a);
+	scanf("%d", &b);
+	scanf("%d", &c);
 	d = b*b - 4*a*c;
 	printf("d = %d \n", d);
-	x1 = (-b + sqrt(d)) / (2*a);
-	x2 = (-b - sqrt(d)) / (2*a);
-	printf("x1 = %lf, x2=%lf",x1,x2);
+	if (d > 0)
+	{
+		x1 = (-b + sqrt(d)) / (2*a);
+		x2 = (-b - sqrt(d)) / (2*a);
+		printf("x1 = %lf, x2=%lf",x1,x2);
+	}
+	if	(d == 0)
+	{
+		x1 = (-b) / (2*a);
+		printf("x1 = %lf", x1, x2);
+	}
+	if (d < 0)
+	{
+		printf("d < 0, no x");
+	}
 	return 0;
 }
--- laba2/laba2.1/main.c
+++ laba2/laba2.1/main.c
@@ -1,29 +1,8 @@
-#pragma hdrstop
 #include <stdio.h>
-#include <conio.h> // Библиотеки с "к лекции 2 contol.cpp"
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
-{
-	char s[80]; // Объявляем переменные
-	int i;
-	if(fgets(s,80,stdin)) // fgets - чтение строки из указанного потока данных,а stdin используется для считывания с консоли.
-	{
-		for(i=0;s[i];i++) // классический цикл перебора
-		{
-			if(s[i]=='a') // проверка символа
-			{
-				s[i]='A'; // Если подходит, то с большой буквы
-			}
-			else
-			{
-				if(s[i]=='b') // проверка
-				{
-					s[i]='B'; // если.....
-				}
-			}
-		}
-	}
-	puts(s); // вывод s
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
 	return 0;
 }
