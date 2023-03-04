6c9bbaf73148cf340bc4cb852921d5fd760c4605
Тарарин Александр <Vapik@stud.vvsu.ru>
2023-03-04 12:59:53 +1000

    Vapik

--- laba3/main.c
+++ laba3/main.c
@@ -3,6 +3,34 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) 
+{
+	char s[80];
+	int i;
+	if(fgets(s,80,stdin))
+	{
+		for(i=0;s[i];i++)
+		{
+			if(s[i]=='a')
+			{
+				s[i]='A';
+			}
+			else
+			{
+				if(s[i]=='b')
+				{
+					s[i]='B';
+				}
+				else
+				{
+					if(s[i]=='!')
+					{
+						s[i]='?';
+					}
+				}
+			}
+		}
+	}
+	puts(s);
 	return 0;
 }
