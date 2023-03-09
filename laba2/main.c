1031bd7b0d2562e44fe5fbca4be3cc619c5a971e
MrVapik <125105502+MrVapik@users.noreply.github.com>
2023-03-09 15:26:45 +1000

    Vapik

--- laba2/main.c
+++ laba2/main.c
@@ -15,17 +15,19 @@ int main(int argc, char *argv[])
 			scanf("%d", &massiv[i][j]);
 		}
 	}
-	double kok = masivv[0][0] + massiv[2][2] + massiv[1][1];
-	double kok = masivv[0][2] + massiv[2][0] + massiv[1][1];
+	double kok = massiv[0][0] + massiv[2][2] + massiv[1][1];
+	double fof = massiv[0][2] + massiv[2][0] + massiv[1][1];
 	printf("Sum main = %d \n", kok);
-	printf("Sum main = %d \n", fof);
-	int d,s,m[2][2]
-	for (d=0;d<2;d++)
+	printf("Sum side = %d \n", fof);
+	int d,s,m[2][2];
+	for (d = 0; d<2;d++)
 	{
 		for(s = 0; s<2; s++)
 		{
-			scnaf("%d", &m[d][s]);
+			scanf("%d", &m[d][s]);
 		}
 	}
+	printf("|%d %d|\n", m[0][0]*m[0][0] + m[0][1]*m[1][0], m[0][0]*m[0][1] + m[0][1]*m[1][1]);
+	printf("|%d %d|", m[0][0]*m[1][0] + m[1][1]*m[1][0], m[1][0]*m[0][1] + m[1][1]*m[1][1]);
 	return 0;
 }
