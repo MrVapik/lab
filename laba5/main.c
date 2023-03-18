b46901884bbb3f5e86e928eb58f55613f17e8ef6
Тарарин Александр <Vapik@stud.vvsu.ru>
2023-03-18 12:58:53 +1000

    Vapik

--- laba5/main.c
+++ laba5/main.c
@@ -1,121 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-// pamit  videlenie
-double** createMatrix(int n)
-{
-	int i;
-    double** matrix = (double**)malloc(n * sizeof(double*));
-    for (i = 0; i < n; i++)
-    {
-        matrix[i] = (double*)malloc(n * sizeof(double));
-    }
-    return matrix;
-}
-
-// pamit osbozdinei
-void freeMatrix(double** matrix, int n)
-{
-	int i;
-    for (i = 0; i < n; i++)
-    {
-        free(matrix[i]);
-    }
-    free(matrix);
-}
-
-// vod matriz
-void inputMatrix(double** matrix, int n)
-{
-	int i,j;
-    for (i = 0; i < n; i++)
-    {
-        for (j = 0; j < n; j++)
-        {
-            scanf("%lf", &matrix[i][j]);
-        }
-    }
-}
-
-// vivod mtriz
-void printMatrix(double** matrix, int n)
-{
-	int i,j;
-    for (i = 0; i < n; i++)
-    {
-        for (j = 0; j < n; j++)
-        {
-            printf("%lf ", matrix[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-// + matriz
-double** addMatrix(double** matrix1, double** matrix2, int n)
-{
-	int i,j;
-    double** result = createMatrix(n);
-    for (i = 0; i < n; i++)
-    {
-        for (j = 0; j < n; j++)
-        {
-            result[i][j] = matrix1[i][j] + matrix2[i][j];
-        }
-    }
-    return result;
-}
-
-// - mtriz
-double** subtractMatrix(double** matrix1, double** matrix2, int n)
-{
-	int i,j;
-    double** result = createMatrix(n);
-    for (i = 0; i < n; i++)
-    {
-        for (j = 0; j < n; j++)
-        {
-            result[i][j] = matrix1[i][j] - matrix2[i][j];
-        }
-    }
-    return result;
-}
-
-// * mtriz
-double** multiplyMatrix(double** matrix1, double** matrix2, int n)
-{
-	int i,j,k;
-    double** result = createMatrix(n);
-    for (i = 0; i < n; i++)
-    {
-        for (j = 0; j < n; j++)
-        {
-            result[i][j] = 0;
-            for (k = 0; k < n; k++)
-            {
-                result[i][j] += matrix1[i][k] * matrix2[k][j];
-            }
-        }
-    }
-    return result;
-}
-
-// vibor oper
-double** performOperation(double** matrix1, double** matrix2, int n, char operation)
-{
-    switch(operation)
-    {
-        case '+':
-            return addMatrix(matrix1, matrix2, n);
-        case '-':
-            return subtractMatrix(matrix1, matrix2, n);
-        case '*':
-            return multiplyMatrix(matrix1, matrix2, n);
-        default:           
-        printf("ne verni znak oper\n");
-        return NULL;
-	}
-}
+#include <conio.h>
+#include <locale.h>
+#include "f.h"
 
 int main()
 {
@@ -132,7 +19,7 @@ printf("2m: ");
 inputMatrix(matrix2, n);
 
 char operation;
-printf("(+, -, *): ");
+printf("(+, -, *, /): ");
 scanf(" %c", &operation);
 
 double** result = performOperation(matrix1, matrix2, n, operation);
@@ -148,4 +35,3 @@ freeMatrix(matrix2, n);
 
 return 0;
 }
-
